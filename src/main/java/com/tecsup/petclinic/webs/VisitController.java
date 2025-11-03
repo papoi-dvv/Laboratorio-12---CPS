f9210762c815aa090c57c60b99902d4da6ecf3d0
papoi-dvv <paulo.santos@tecsup.edu.pe>
2025-11-03 17:32:02 -0500

    VisitControllerTest

--- src/main/java/com/tecsup/petclinic/webs/VisitController.java
+++ src/main/java/com/tecsup/petclinic/webs/VisitController.java
@@ -1,4 +1,75 @@
 package com.tecsup.petclinic.webs;
 
+import com.tecsup.petclinic.dtos.VisitDTO;
+import com.tecsup.petclinic.exceptions.VisitNotFoundException;
+import com.tecsup.petclinic.mapper.VisitMapper;
+import com.tecsup.petclinic.services.VisitService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Slf4j
 public class VisitController {
+
+	private final VisitService visitService;
+	private final VisitMapper mapper;
+
+	public VisitController(VisitService visitService, VisitMapper mapper) {
+		this.visitService = visitService;
+		this.mapper = mapper;
+	}
+
+	@GetMapping("/visits")
+	public ResponseEntity<List<VisitDTO>> findAll() {
+		List<VisitDTO> visits = visitService.findAll();
+		return ResponseEntity.ok(visits);
+	}
+
+	@PostMapping("/visits")
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<VisitDTO> create(@RequestBody VisitDTO visitDTO) {
+		VisitDTO newVisit = visitService.create(visitDTO);
+		return ResponseEntity.status(HttpStatus.CREATED).body(newVisit);
+	}
+
+	@GetMapping("/visits/{id}")
+	public ResponseEntity<VisitDTO> findById(@PathVariable Integer id) {
+		try {
+			VisitDTO dto = visitService.findById(id);
+			return ResponseEntity.ok(dto);
+		} catch (VisitNotFoundException e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@PutMapping("/visits/{id}")
+	public ResponseEntity<VisitDTO> update(@RequestBody VisitDTO visitDTO, @PathVariable Integer id) {
+		try {
+			VisitDTO existing = visitService.findById(id);
+
+			existing.setPetId(visitDTO.getPetId());
+			existing.setVisitDate(visitDTO.getVisitDate());
+			existing.setDescription(visitDTO.getDescription());
+
+			visitService.update(existing);
+			return ResponseEntity.ok(existing);
+		} catch (VisitNotFoundException e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@DeleteMapping("/visits/{id}")
+	public ResponseEntity<String> delete(@PathVariable Integer id) {
+		try {
+			visitService.delete(id);
+			return ResponseEntity.ok("Delete ID :" + id);
+		} catch (VisitNotFoundException e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
 }
--- src/test/java/com/tecsup/petclinic/webs/VisitControllerTest.java
+++ src/test/java/com/tecsup/petclinic/webs/VisitControllerTest.java
@@ -25,18 +25,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Slf4j
 public class VisitControllerTest {
 
-    @Autowired
-    private ObjectMapper om;
+        @Autowired
+        private ObjectMapper om;
 
 
-    @Autowired
-    private MockMvc mockMvc;
+        @Autowired
+        private MockMvc mockMvc;
     @Test
     public void testCreateVisit() throws Exception {
 
         VisitDTO newVisit = new VisitDTO();
-        newVisit.setPetId(1L);
-        newVisit.setVisitDate(LocalDate.of(2025, 11, 3));
+        newVisit.setPetId(1);
+        newVisit.setVisitDate("2025-11-03");
         newVisit.setDescription("Control general");
 
         this.mockMvc.perform(post("/visits")
@@ -50,8 +50,8 @@ public class VisitControllerTest {
     @Test
     public void testFindVisitOK() throws Exception {
         VisitDTO visitDTO = new VisitDTO();
-        visitDTO.setPetId(2L);
-        visitDTO.setVisitDate(LocalDate.of(2025, 11, 3));
+        visitDTO.setPetId(2);
+        visitDTO.setVisitDate("2025-11-03");
         visitDTO.setDescription("Vacunación");
 
         ResultActions mvcActions = mockMvc.perform(post("/visits")
@@ -60,7 +60,7 @@ public class VisitControllerTest {
                 .andExpect(status().isCreated());
 
         String response = mvcActions.andReturn().getResponse().getContentAsString();
-        Integer id = JsonPath.parse(response).read("$.id");
+        Integer id = JsonPath.parse(response).read("$.id", Integer.class);
         this.mockMvc.perform(get("/visits/" + id))
                 .andDo(print())
                 .andExpect(status().isOk())
@@ -77,8 +77,8 @@ public class VisitControllerTest {
     @Test
     public void testUpdateVisit() throws Exception {
         VisitDTO newVisit = new VisitDTO();
-        newVisit.setPetId(3L);
-        newVisit.setVisitDate(LocalDate.of(2025, 11, 3));
+        newVisit.setPetId(3);
+        newVisit.setVisitDate("2025-11-03");
         newVisit.setDescription("visita ini");
 
         ResultActions mvcActions = mockMvc.perform(post("/visits")
@@ -87,11 +87,11 @@ public class VisitControllerTest {
                 .andExpect(status().isCreated());
 
         String response = mvcActions.andReturn().getResponse().getContentAsString();
-        Integer id = JsonPath.parse(response).read("$.id");
+        Integer id = JsonPath.parse(response).read("$.id", Integer.class);
         VisitDTO updatedVisit = new VisitDTO();
-        updatedVisit.setId(Long.valueOf(id));
-        updatedVisit.setPetId(3L);
-        updatedVisit.setVisitDate(LocalDate.of(2025, 11, 3));
+        updatedVisit.setId(id);
+        updatedVisit.setPetId(3);
+        updatedVisit.setVisitDate("2025-11-03");
         updatedVisit.setDescription("Chequeo de control");
 
         mockMvc.perform(put("/visits/" + id)
@@ -104,8 +104,8 @@ public class VisitControllerTest {
     @Test
     public void testDeleteVisit() throws Exception {
         VisitDTO visitDTO = new VisitDTO();
-        visitDTO.setPetId(4L);
-        visitDTO.setVisitDate(LocalDate.of(2025, 11, 3));
+        visitDTO.setPetId(4);
+        visitDTO.setVisitDate("2025-11-03");
         visitDTO.setDescription("Desparasitación");
 
         ResultActions mvcActions = mockMvc.perform(post("/visits")
